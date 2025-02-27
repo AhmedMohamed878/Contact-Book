78a4c524950105a5fbad4bf597f10c62f67a054b
AhmedMohamed878 <am3753828@gmail.com>
2025-02-27 18:00:47 +0200

    Add files via upload

---
+++ Contact Book.py
@@ -0,0 +1,13 @@
+contacts= dict(Ahmed="01000406933",Ali="0029501-01939", Dona="00284789747")
+print(contacts)
+
+name=str(input("Enter the name: "))
+number = contacts.get(name)
+    
+if name in contacts:
+       
+    print("The phone number of: " + name + " is " + number)
+       
+    
+else:
+        print("Invalid name")
